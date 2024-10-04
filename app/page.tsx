ddf99afe591405a4d9aa8db828443ba3c17baf81
İsmail ÇAKMAK <werduet@hotmail.com>
2024-10-04 15:53:20 +0300

    test11

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
       </div>
       <div className="flex justify-center items-center my-5 border-t-2 p-2">
         <h1 className="text-2xl w-full text-center font-Nunito font-bold">
-          Yardımları için <span className="text-red-500">KARACA</span> 'ya teşekkür ederiz.
+          Yardımları için <span className="text-red-500">KARACA</span> ya teşekkür ederiz.
         </h1>
       </div>
     </main>
